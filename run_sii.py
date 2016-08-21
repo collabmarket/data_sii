93853514905d0264f3f767e940d8f64cea320c58
jrovegno <javier.rovegno@gmail.com>
2016-08-20 21:47:48 -0300

    CSV excel spanish format

--- run_sii.py
+++ run_sii.py
@@ -12,6 +12,11 @@ update_all_years = sii.yesno(prompt = u'¿Actualizar datos históricos?',
 update_last_year = sii.yesno(prompt = u'¿Actualizar año presente?', 
                             default = 'y')
 
+# Opciones lectura de archivos csv compatibles con excel spanish
+karg_csvr = dict(delimiter=';', decimal=',', index_col=0, parse_dates=True)
+# Opciones escrituda de archivos csv compatibles con excel spanish
+karg_csvw = dict(sep=';', decimal=',')
+
 if update_all_years:
     dfs = []
     yr = date.today().year
@@ -19,16 +24,16 @@ if update_all_years:
         df = sii.get_data(year)
         dfs.append(df)
     uf = pd.concat(dfs)
-    uf.to_csv('uf.csv')
+    uf.to_csv('uf.csv', **karg_csvw)
     print uf.info()
 
 if update_last_year:
-    uf = pd.read_csv('uf.csv', parse_dates=True, index_col=0)
+    uf = pd.read_csv('uf.csv', **karg_csvr)
     yr = date.today().year
     df = sii.get_data(yr)
     uf = pd.concat([uf,df])
     # Drop extra copy of duplicate index of Pandas Series
     uf = uf.groupby(uf.index).last()
-    uf.to_csv('uf.csv')
+    uf.to_csv('uf.csv', **karg_csvw)
     print uf.info()
 
